048f32ba82259058a16f9b807117f3ae3196cfec
Hamzah <ahmadhamzah500@gmail.com>
2020-01-20 22:18:39 -0600

    Added code for heroku deployment

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
 const bodyParser = require("body-parser");
+const path = require("path");
 var cors = require("cors");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 const db = config.get("mongoURI");
@@ -20,4 +22,14 @@ mongoose
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 
-app.listen(5000, () => console.log("Server running on port 3000"));
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
